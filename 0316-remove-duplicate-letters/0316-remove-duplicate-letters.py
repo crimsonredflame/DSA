518dc5f1fd4188cccb86f6f71bc9999fd354334c
crimsonredflame <tanisha7565@gmail.com>
2026-07-19 15:48:54 +0530

    Time: 3 ms (70%), Space: 12.8 MB (8.79%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vis = [False]*26
+        last = [-1]*26
+        l = len(s)
+        st = []
+        for i in range(l) :
+            last[ord(s[i]) - ord('a')] = i
+        for i in range(l) :
+            if vis[ord(s[i]) - ord('a')] == False : 
+                while st :
+                    if ord(st[-1]) > ord(s[i]) and last[ord(st[-1]) - ord('a')] > i :
+                        vis[ord(st[-1]) - ord('a')] = False
+                        st.pop()
+                    else : break
+                st.append(s[i])
+                vis[ord(s[i]) - ord('a')] = True
+        return ''.join(st)
