59dee9ed57e3dfa898f78d2cda365239c4e7b261
crimsonredflame <tanisha7565@gmail.com>
2026-09-14 01:51:54 +0530

    Time: 0 ms (100%), Space: 13 MB (20.41%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -5,20 +5,38 @@ class Solution(object):
         :type graph: List[List[int]]
         :rtype: bool
         """
-        l = len(graph)
-        q = deque()
-        colArr = [-1]*l
+
+        #BFS
+        # l = len(graph)
+        # q = deque()
+        # colArr = [-1]*l
         
+        # #Always check for the discontinued graph
+        # for j in range(l) :
+        #     if colArr[j] == -1 :
+        #         q.append(j)
+        #         colArr[0] = 0
+        #         while q :
+        #             node = q.popleft()
+        #             for i in graph[node] :
+        #                 if colArr[i] == -1 :
+        #                     colArr[i] = 1 - colArr[node]
+        #                     q.append(i)
+        #                 elif colArr[i] == colArr[node] :
+        #                     return False
+        # return True
+
+        #DFS
+        l = len(graph)
+        vis = [-1]*l
+        def dfs(node,col) :
+            vis[node] = col
+            for i in graph[node] :
+                if vis[i] == -1 :
+                    if dfs(i,1-col) == False : return False
+                elif vis[i] == vis[node] :
+                    return False
         for j in range(l) :
-            if colArr[j] == -1 :
-                q.append(j)
-                colArr[0] = 0
-                while q :
-                    node = q.popleft()
-                    for i in graph[node] :
-                        if colArr[i] == -1 :
-                            colArr[i] = 1 - colArr[node]
-                            q.append(i)
-                        elif colArr[i] == colArr[node] :
-                            return False
+            if vis[j] == -1 :
+                if dfs(j,0) == False : return False
         return True
